ee4127153a7264132ba9c487e823d4547e22ac22
Petr Zajac <petr.zajac@gmail.com>
2013-10-28 13:57:20 +0100

    Unused import removed

--- src/test/java/org/zajo/simplex/solver/PivotSolverTest.java
+++ src/test/java/org/zajo/simplex/solver/PivotSolverTest.java
@@ -2,7 +2,6 @@ package org.zajo.simplex.solver;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
